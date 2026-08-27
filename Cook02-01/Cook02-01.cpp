2a11cc0290832ff55ef38fb8c83e9b1a512ed9c3
rlaehddud <6070kjh475@naver.com>
2026-08-27 10:02:27 +0900

    .

---
+++ Cook02-01/Cook02-01.cpp
@@ -0,0 +1,40 @@
+//#define print(data) cout << data << " "
+//#define println(data) \ 
+			//	cout << data << endl
+using namespace std;
+#include <iostream>
+#include <string>
+//#define input(data,msg) \
+//	cout << msg; \
+//	cin >> data
+#define println(data) cout << data << endl	
+int main() {
+	string str1 = "IT CookBook";
+	const char* char1 = "자료구조와 알고리즘";
+	println(str1);
+	println(char1);
+
+	string str2 = char1;
+	println(str2);
+	const char* char2 = str1.c_str();
+	println(char2);
+
+	println(str1.size());
+	println(((string)char1).size());
+
+	str1 = "100";
+	char1 = "200";
+	println(str1 + char1);
+
+	int value = stoi(str1) + stoi(char1);
+	println("합계는 " + to_string(value));
+//	print("안녕하세요?");
+	//println(100);
+	//print("또 안녕하세요?");
+	//print(3.14);
+	/*string strData;
+	int intData;
+	input(strData, "문자열을 입력하세요: ");
+	input(intData, "정수를 입력하세요: ");*/
+
+}
